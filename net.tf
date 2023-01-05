1dba06699b8724a544f636f2f93e9ada60cf8542
rcalvogarcia <79523420+rcalvogarcia@users.noreply.github.com>
2023-01-05 12:57:26 +0100

    añado net qa

--- net.tf
+++ net.tf
@@ -1,1 +1,11 @@
 #test
+resource "azurerm_virtual_network" "Learn_Terraform_Vnet-qa" {
+
+  name                = "vnet-qa"
+  address_space       = ["10.1.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.Terraform-Learn.name
+  depends_on = [
+    azurerm_resource_group.Terraform-Learn
+  ]
+}
