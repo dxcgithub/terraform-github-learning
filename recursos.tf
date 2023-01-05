beea44325eab3133c120c1db6b88c62a5f7db98a
rcalvogarcia <79523420+rcalvogarcia@users.noreply.github.com>
2023-01-05 13:17:31 +0100

    3 redes

--- recursos.tf
+++ recursos.tf
@@ -2,7 +2,7 @@
 #test3
 
 resource "azurerm_resource_group" "Terraform-Learn" {
-  name     = "testTC"
+  name     = "DXCMOVE"
   location = "eastus"
 
 }
@@ -11,6 +11,26 @@ resource "azurerm_virtual_network" "Learn_Terraform_Vnet-TS" {
 
   name                = "vnet-test"
   address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.Terraform-Learn.name
+  depends_on = [
+    azurerm_resource_group.Terraform-Learn
+  ]
+}
+resource "azurerm_virtual_network" "Learn_Terraform_Vnet-QA" {
+
+  name                = "vnet-qa"
+  address_space       = ["10.1.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.Terraform-Learn.name
+  depends_on = [
+    azurerm_resource_group.Terraform-Learn
+  ]
+}
+resource "azurerm_virtual_network" "Learn_Terraform_Vnet-PROD" {
+
+  name                = "vnet-prod"
+  address_space       = ["10.2.0.0/16"]
   location            = "eastus"
   resource_group_name = azurerm_resource_group.Terraform-Learn.name
   depends_on = [
